0fa09e639009d958e0970bb5325efb668e65b635
Shathursan <sathurshans04@gmail.com>
2024-07-19 11:31:41 +0530

    first commit

---
+++ src/test/java/Spring/basic/crud/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.basic.crud.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
